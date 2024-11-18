75f19f3fc1f15e667ca0e33113786ba4791c760a
JANAK <98094845+janaksanjel@users.noreply.github.com>
2024-11-18 21:53:07 +0545

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,14 +2,12 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://janaksa:9840748285@orderonline.xnn0w.mongodb.net/', {
-      useNewUrlParser: true,  // Ensures use of the new MongoDB connection string parser
-      useUnifiedTopology: true,  // Enables the unified topology layer
-      dbName: 'test',  // Replace with your specific database name
+    await mongoose.connect('mongodb+srv://janaksa:9840748285@orderonline.xnn0w.mongodb.net/test', {
+      dbName: 'test', // Replace with the name of your database
     });
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error('Error connecting to MongoDB:', err.message);
-    process.exit(1); // Exit process with failure if connection fails
+    process.exit(1);
   }
 };
