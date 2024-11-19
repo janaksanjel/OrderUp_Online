5778e584d8fba585a3b482f15ff79efedc96f214
JANAK <98094845+janaksanjel@users.noreply.github.com>
2024-11-19 07:24:38 +0545

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,17 +1,6 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-// Load environment variables
-dotenv.config();
 
 export const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      dbName: 'test', // Specify the database name
-    });
-    console.log('MongoDB Connected...');
-  } catch (err) {
-    console.error('Error connecting to MongoDB:', err.message);
-    process.exit(1);
-  }
-};
+    await mongoose.connect('mongodb+srv://janaksa:9840748285@orderonline.xnn0w.mongodb.net/')
+        .then(() => console.log('MongoDB Connected...'))
+}
